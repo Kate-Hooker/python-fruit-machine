d03c5b0c5e45c4955e3b0aef0149be55e1c7232f
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-21 20:05:17 +1300

    else statement added if deposit is less than zero ask for a real number, else added to deal with a non-numerical deposit

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ def deposit():
     amount = input("Enter amount to deposit: $")
     if amount.isdigit():
       amount = int(amount)
+      if amount > 0:
+        break
+      else:
+        print("Please pick an amount bigger than zero")
+    else:
+      print("Please enter a number")
